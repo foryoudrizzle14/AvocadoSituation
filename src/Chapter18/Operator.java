4a5dbe37927bb16e4bb1b78564bb2014e1fed12f
Boseul <115998794+foryoudrizzle14@users.noreply.github.com>
2024-10-29 23:01:16 +0900

    단항연상자

---
+++ src/Chapter18/Operator.java
@@ -0,0 +1,23 @@
+package Chapter18;
+
+public class Operator {
+
+    public static void main(String[] args) {
+
+        //+=, *=
+
+//        int myInt = 5;
+//        myInt += 1;
+//        System.out.println(myInt);
+//        int myInt = 5;
+//        myInt *= 10;
+//        System.out.println(myInt);
+
+
+        //단항 연산자
+     int myInt2 = 10;
+     myInt2++;
+     System.out.println(myInt2);
+    }
+
+}
